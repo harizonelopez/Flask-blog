532ac09d488dc8fc69b7e94139b0388f9ea294c2
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-02-28 14:16:48 +0300

    created main

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("OH")
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    posts = Post.query.order_by(Post.date_posted.desc()).all()
+    return render_template('home.html', posts=posts)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    
+    return render_template('index.html') 
+
+if __name__ == '__main__':
+    app.run(debug=True)
